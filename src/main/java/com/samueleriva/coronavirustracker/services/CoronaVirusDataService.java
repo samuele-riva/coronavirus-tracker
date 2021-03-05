fcfd7cd04f927735bb4c5064fac240187db7e078
samuele-riva <s.riva72@campus.unimib.it>
2021-03-05 12:30:24 +0100

    UI first version

--- src/main/java/com/samueleriva/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/samueleriva/coronavirustracker/services/CoronaVirusDataService.java
@@ -23,6 +23,10 @@ public class CoronaVirusDataService {
 
     private List<LocationStats> allStats = new ArrayList<>();
 
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
@@ -48,4 +52,5 @@ public class CoronaVirusDataService {
         this.allStats = newStats;
 
     }
+
 }
